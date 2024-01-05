b5acafd559a68e46b6c7657757aec07091caea3e
yehor393 <yehor393@gmail.com>
2024-01-04 19:55:47 -0700

    Jan 4

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Yehor',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'Dec 28, 2023'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'Dec 29, 2023'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
